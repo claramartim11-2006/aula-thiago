641f9fbb3852674cc96cb76b9bca73255a3b9d77
claramartim11-2006 <claramartim11@gmail.com>
2026-03-16 20:13:19 +0000

    16/03/26

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 import Titulo from './components/Titulo'
+import Aluno from './components/Aluno'
+
+function mostrarMensagem(){
+  alert("Aluno não cadastrado!")
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
+    <>
       <div>
         <Titulo/>
+        <Aluno/>
       </div>
+      <br></br>
+      <div>
+        <button onClick={mostrarMensagem}> Verificar matricula</button>
+      </div>
+    </>
   )
 }
 
---
+++ my-app/src/components/Aluno.jsx
@@ -0,0 +1,8 @@
+function Aluno() {
+    return(
+        <div>
+            <p>Aluno: Clara</p>
+        </div>
+    )
+}
+export default Aluno
